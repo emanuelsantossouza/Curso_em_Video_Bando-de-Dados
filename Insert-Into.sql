35c491ae4301b16d36e32c24596fd4a172a81f4d
emanuel santoss <emanuelsantoss42@gmail.com>
2022-11-20 20:33:29 -0300

    Tabela de insert-into
    Criando os insert into da tabela

---
+++ Insert-Into.sql
@@ -0,0 +1,13 @@
+insert into pessoa 
+values	(default,'Joana', '1984-11-01', 'F', '111.5','1.90', 'hilarnzda');
+
+insert into pessoa 		
+values	(default,'Gordofredo', '1984-01-02', 'M', '78.5','1.83','Brasil');
+
+insert into pessoa 
+values	(default,'Maria', '1984-11-01', 'F', '80.5','1.50',default);
+
+insert into pessoa 
+values	(default,'Ana', '1984-11-01', 'F', '80.5','1.50',default),
+	(default,'Ana Carol', '1984-4-01', 'F', '70.5','1.20',default),
+	(default,'Nikita', '1984-11-09', 'F', '38','1.70',default);
