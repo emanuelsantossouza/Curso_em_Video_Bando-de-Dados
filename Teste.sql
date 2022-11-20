8578e03981176497887efa1a68a5bb7feb7eeb92
emanuel santoss <emanuelsantoss42@gmail.com>
2022-11-20 20:42:53 -0300

    Tabela-teste
    Desenvolvida para teste durante a aula

---
+++ Teste.sql
@@ -0,0 +1,12 @@
+create table if not exists Tesete (
+id int,
+nome varchar(20),
+idade int
+);
+
+insert into Tesete value 
+('1', 'Carlos','10'),
+('2', 'Joana','90'),
+('3','Pedro','32');
+
+select * from Tesete;
